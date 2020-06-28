f3508e7a7cb56a0f109c5da27dbb8801b178c9de
7101981 <43120539+7101981@users.noreply.github.com>
2020-06-28 11:52:09 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Deposit from './components/Deposit';
+import Withdraw from './components/Withdraw';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="app-header">
+          <ul className="ul-style">
+            <li className="li-style">
+              <NavLink to="/" className="App-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="li-style">
+              <NavLink to="/deposit" className="App-link">
+                Deposit
+              </NavLink>
+            </li>
+            <li className="li-style">
+              <NavLink to="/withdraw" className="App-link">
+                Withdraw
+              </NavLink>
+            </li>
+          </ul>
+
+          <Route path='/' exact component={Home} />
+          <Route path='/deposit' component={Deposit} />
+          <Route path='/withdraw' component={Withdraw} />
+        </header>
+      </div>
+    </Router>
+
   );
 }
 
